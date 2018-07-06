52244a2029c33aa3684b3cd357ae559bdf60945c
YutaOikawa <oikenfight@gmail.com>
2018-07-06 15:29:22 +0900

    create Operator class, which operate android by adb command.

---
+++ app/Console/Commands/Operator.php
@@ -0,0 +1,198 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+/**
+ * Class Operation
+ * @package App\Console\Commands
+ */
+final class Operator
+{
+    const SCREEN_X_SIZE = 1020;
+
+    const SCREEN_Y_SIZE = 1900;
+
+    const ADB_STORAGE_DIR = '/sdcard/';
+
+    const LOCAL_STORAGE_DIR = './storage/';
+
+    const POST_URL = 'http://127.0.0.1:8080/api/screen_shot';
+
+    /**
+     * @var int
+     */
+    protected $maxX;
+
+    /**
+     * @var int
+     */
+    protected $maxY;
+
+    /**
+     * @var int
+     */
+    protected $x;
+
+    /**
+     * @var int
+     */
+    protected $y;
+
+    /**
+     * @var int
+     */
+    protected $toX;
+
+    /**
+     * @var int
+     */
+    protected $toY;
+
+    /**
+     * @param int $maxX
+     * @param int $maxY
+     * @return void
+     */
+    public function setMaxXY(int $maxX, int $maxY)
+    {
+        $this->maxX = $maxX;
+        $this->maxY = $maxY;
+    }
+
+    /**
+     * @param $x
+     * @param $y
+     * @return void
+     */
+    public function setXY($x, $y)
+    {
+        $this->x = intval(($x / $this->maxX) * self::SCREEN_X_SIZE);
+        $this->y = intval(($y / $this->maxY) * self::SCREEN_Y_SIZE);
+    }
+
+    /**
+     * @param $toX
+     * @param $toY
+     * @return void
+     */
+    public function setToXY($toX, $toY)
+    {
+        $this->toX = intval(($toX / $this->maxX) * self::SCREEN_X_SIZE);
+        $this->toY = intval(($toY / $this->maxY) * self::SCREEN_Y_SIZE);
+    }
+
+    /**
+     * 端末の画面が Off の場合 On にする（ロックなしの場合のみ）
+     *
+     * @return void
+     */
+    public function turnOnIfDisplayPowerOff()
+    {
+        $cmd = 'adb shell dumpsys power | grep state=';
+        exec($cmd, $output, $return_var);
+        if (!strpos($output[0], 'ON')) {
+            exec('adb shell input keyevent 82');
+            sleep(2);
+        }
+    }
+
+    /**
+     * 座標 (x, y) をタップする
+     *
+     * @return void
+     */
+    public function tap()
+    {
+        \Log::debug('tap');
+        $cmd = sprintf("adb shell input touchscreen tap %s %s", $this->x, $this->y);
+        exec($cmd, $output, $return_var);
+        sleep(1);
+    }
+
+    /**
+     * 座標 (x, y) から 座標 (toX, toY) にスワイプする
+     *
+     * @return void
+     */
+    public function swipe()
+    {
+        \Log::debug('swipe');
+        $cmd = sprintf("adb shell input swipe %s %s %s %s", $this->x, $this->y, $this->toX, $this->toY);
+        exec($cmd, $output, $return_var);
+        sleep(1);
+    }
+
+    /**
+     * @return bool|int
+     */
+    public function findKeyboard()
+    {
+        \Log::debug('check keyboard is shown');
+        $cmd = 'adb shell dumpsys input_method | grep mInputShown';
+        exec($cmd, $output, $return_var);
+        return strpos($output[0], 'true');
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return int
+     */
+    public function input(string $text)
+    {
+        \Log::debug('input text');
+        exec('adb shell input text ' . $text, $output, $return_var);
+        sleep(1);
+        return $return_var;
+    }
+
+    /**
+     * スクリーンショットを撮影
+     *
+     * @return void
+     */
+    public function screenShot(string $name)
+    {
+        $this->takeAScreenShot($name);
+        $this->pullScreenShotToLocal($name);
+        $this->removeUsedFile($name);
+    }
+
+    /**
+     * @param string $name
+     */
+    protected function takeAScreenShot(string $name)
+    {
+        \Log::debug('screen shot');
+        $adbScreenShotPath = self::ADB_STORAGE_DIR . $name;
+        exec(sprintf("adb shell screencap -p %s", $adbScreenShotPath), $output, $return_var);
+
+        return $return_var;
+    }
+
+    /**
+     * @param string $name
+     */
+    protected function pullScreenShotToLocal(string $name)
+    {
+        \Log::debug('pull screen shot');
+        $adbScreenShotPath = self::ADB_STORAGE_DIR . $name;
+        $localScreenShotPath = self::LOCAL_STORAGE_DIR . $name;
+        exec(sprintf("adb pull %s %s", $adbScreenShotPath, $localScreenShotPath), $output, $return_var);
+        return $return_var;
+    }
+
+    /**
+     * @param string $name
+     */
+    protected function removeUsedFile(string $name)
+    {
+        \Log::debug('remove unused screen shot');
+        $adbScreenShotPath = self::ADB_STORAGE_DIR . $name;
+        $localScreenShotPath = self::LOCAL_STORAGE_DIR . $name;
+        // exec(sprintf("adb shell rm %s && rm %s", $adbScreenShotPath, $localScreenShotPath));
+        exec(sprintf("adb shell rm %s", $adbScreenShotPath), $output, $return_var);
+        return $return_var;
+    }
+}
