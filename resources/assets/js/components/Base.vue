449cc27013e95583bfbf316dd8b97a9a5693a250
YutaOikawa <oikenfight@gmail.com>
2018-07-06 15:35:21 +0900

    remove it from Base component.

--- resources/assets/js/components/Base.vue
+++ resources/assets/js/components/Base.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="row justify-content-center">
-        <div class="col-10 clearfix" style="padding-top: 40px;">
-            <!-- TASK: これ上手くできなかった。。 -->
-            <div class="float-right">
-                <div class="row">
-                    <div>
-                        <button @click="startUpload" class="btn btn-lg btn-outline-primary" :class="{active: uploading}">スタート</button>
-                    </div>
-                    <div style="padding-left: 10px;">
-                        <button @click="stopUpload" class="btn btn-lg btn-outline-secondary" :class="{active: !uploading}" aria-pressed="true">ストップ</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <!--<div class="col-10 clearfix" style="padding-top: 40px;">-->
+            <!--&lt;!&ndash; TASK: これ上手くできなかった。。 &ndash;&gt;-->
+            <!--<div class="float-right">-->
+                <!--<div class="row">-->
+                    <!--<div>-->
+                        <!--<button @click="startUpload" class="btn btn-lg btn-outline-primary" :class="{active: uploading}">スタート</button>-->
+                    <!--</div>-->
+                    <!--<div style="padding-left: 10px;">-->
+                        <!--<button @click="stopUpload" class="btn btn-lg btn-outline-secondary" :class="{active: !uploading}" aria-pressed="true">ストップ</button>-->
+                    <!--</div>-->
+                <!--</div>-->
+            <!--</div>-->
+        <!--</div>-->
 
         <router-view></router-view>
     </div>
